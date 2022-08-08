1fce9b263dee790cd40706bef99e14a0c5990a39
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2022-08-08 10:28:32 +0200

    feat: Infrastructure created

---
+++ DevFreelaOne.Infrastructure/Persistence/DevFreelaOneDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DevFreelaOne.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreelaOne.Infrastructure.Persistence
+{
+    public class DevFreelaOneDbContext : DbContext
+    {
+        public DevFreelaOneDbContext(DbContextOptions<DevFreelaOneDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
+        public DbSet<ProjectComment> Comments { get; set; }
+    }
+}
